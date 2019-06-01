44872991bcdf75bcc75ee777bbddbb1bd8f86870
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2019-06-01 14:08:48 +0300

    Color picker

--- src/window.py
+++ src/window.py
@@ -1,11 +1,24 @@
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
 
 from src.gui import Ui_MainWindow
 
 
-class Window(QtWidgets.QMainWindow):
+class Window(QMainWindow):
 
     def __init__(self):
+
         super(Window, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.curr_color = '#000'
+        self.ui.color_button.setStyleSheet('background-color: ' + self.curr_color + ';')
+
+        self.ui.color_button.clicked.connect(self.pick_color)
+
+    def pick_color(self):
+
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.curr_color = color.name()
+            self.ui.color_button.setStyleSheet('background-color: ' + self.curr_color + ';')
